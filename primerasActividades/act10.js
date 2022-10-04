3c38ca82226f7cc3bd1080f6463ddc9c4107166a
bloid3 <pablo.pereira00@gmail.com>
2022-10-04 12:01:02 +0200

    410

--- primerasActividades/act10.js
+++ primerasActividades/act10.js
@@ -1,9 +1,11 @@
 let notas = prompt("Introduce notas, separados por comas")
 let lista = notas.split(",")
 let comprobar = false
-for (let i = 0; i < lista.length; i++) {
+let i = 0
+while (lista[i] != -1) {
 	if (lista[i] == 10)
 		comprobar = true
+	i++
 }
 if (comprobar == true)
 	document.write("SI")
