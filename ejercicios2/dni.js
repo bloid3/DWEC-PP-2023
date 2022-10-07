b17a63e122ce904b19256e329e2d0133a20079ca
bloid3 <pablo.pereira00@gmail.com>
2022-10-07 09:51:24 +0200

    Subiendo código

--- ejercicios2/dni.js
+++ ejercicios2/dni.js
@@ -1,3 +1,11 @@
 const letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L',
 'C', 'K', 'E']
 
+var idA = setInterval("dni()", 20000)
+
+function dni() {
+	var dni = parseint(prompt("Introduzca un dni sin letra"))
+	if (dni == -1)
+		clearInterval(idA)
+	
+}
