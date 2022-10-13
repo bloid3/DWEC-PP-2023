cae83ab327c90b768f492fa544b3d8ac2578abf6
bloid3 <pablo.pereira00@gmail.com>
2022-10-13 13:17:12 +0200

    Subiendo código

--- ejercicios2/4-1.js
+++ ejercicios2/4-1.js
@@ -5,6 +5,12 @@ while (i < 10) {
 	nombres.push(nombre)
 	i++;
 }
-document.write(nombres)
+document.write(`${nombres} `)
 document.write('<br>')
 document.write(`Hay ${nombres.length} nombres`)
+document.write('<br>')
+let a = nombres.sort()
+document.write(a)
+document.write('<br>')
+let b = nombres.reverse()
+document.write(b)
