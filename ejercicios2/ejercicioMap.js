265d3a5d024c8731ca0e29c04abcf4d52bfaeecc
bloid3 <pablo.pereira00@gmail.com>
2022-10-18 11:56:34 +0200

    Subiendo código

---
+++ ejercicios2/ejercicioMap.js
@@ -0,0 +1,8 @@
+const iesgoya = [
+	{clave: "DWECL", nombre: "Desarrollo Web en Entorno Cliente"},
+	{clave: "DAW", nombre: "Despliegue de Aplicaciones Web"},
+	{clave: "EIE", nombre: "Empresa e Iniciativa Emprendedora"},
+	{clave: "ING", nombre: "Inglés"},
+	{clave: "DES", nombre: "Desarrollo en Entorno Servidor"},
+	{clave: "INTERFACES", nombre: "Desarrollo de Interfaces Web"}
+]
