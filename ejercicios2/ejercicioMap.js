d4111f058c720841c3d8116291adb04d138dd413
bloid3 <pablo.pereira00@gmail.com>
2022-10-18 12:30:56 +0200

    Subiendo código

--- ejercicios2/ejercicioMap.js
+++ ejercicios2/ejercicioMap.js
@@ -1,8 +1,32 @@
-const iesgoya = [
-	{clave: "DWECL", nombre: "Desarrollo Web en Entorno Cliente"},
-	{clave: "DAW", nombre: "Despliegue de Aplicaciones Web"},
-	{clave: "EIE", nombre: "Empresa e Iniciativa Emprendedora"},
-	{clave: "ING", nombre: "Inglés"},
-	{clave: "DES", nombre: "Desarrollo en Entorno Servidor"},
-	{clave: "INTERFACES", nombre: "Desarrollo de Interfaces Web"}
-]
+const iesgoya = new Map([
+	[ "DWECL",  "Desarrollo Web en Entorno Cliente"],
+	[ "DAW",  "Despliegue de Aplicaciones Web"],
+	[ "EIE",  "Empresa e Iniciativa Emprendedora"],
+	[ "ING",  "Inglés"],
+	[ "DES",  "Desarrollo en Entorno Servidor"],
+	[ "INTERFACES",  "Desarrollo de Interfaces Web"]
+]);
+console.log(`Hay ${iesgoya.size} elementos`)
+iesgoya.forEach (function(key,value) {
+	console.log(`${key}: ${value}`)
+})
+console.log(iesgoya.keys())
+
+let nombres = iesgoya.values()
+for (let nombre of nombres) {
+	console.log(nombre)
+	nombre++
+}
+
+
+borrar()
+
+function borrar() {
+	let clave = prompt("Introduzca la clave del módulo que quieres borrar")
+	if (iesgoya.has(clave)) {
+		iesgoya.delete(clave)
+		console.log("El módulo ha sido borrado")
+	} else {
+		console.log("Ese módulo no existe")
+	}
+}
