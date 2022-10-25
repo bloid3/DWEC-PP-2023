faae3cada693f90431852fa52a3083883e373b25
bloid3 <pablo.pereira00@gmail.com>
2022-10-25 13:04:08 +0200

    Subiendo código

---
+++ ud4/4_4.js
@@ -0,0 +1,18 @@
+document.getElementById("input2")
+document.getElementsByTagName("input")[2]
+
+document.getElementsByTagName("p")
+document.querySelectorAll('p')
+
+document.getElementById("lipsum").getElementsByTagName("p")
+document.querySelector('#lipsum p')
+
+document.getElementsByTagName("form")[0]
+document.forms[0]
+
+document.querySelectorAll('input')
+document.getElementsByTagName("input")
+
+document.querySelectorAll('input[name="sexo"]')
+
+document.querySelectorAll('li.important')
