3c4e087b67a0a08acc00840b40393aaebf2f4821
bloid3 <pablo.pereira00@gmail.com>
2022-10-28 09:37:01 +0200

    Subiendo código

--- ud4/4-8.js
+++ ud4/4-8.js
@@ -1,8 +1,12 @@
+
 function aficiones() {
 	let x = document.getElementsByTagName("input");
-	for (let i = 0; i < x.length; i++) {
-		if (x[i] != "") {
-			document.getElementsByTagName("div")[`${i}`].appendChild(document.createElement("p")).innerHTML = x[i].value;
+	if (document.querySelectorAll("div p").length == 0) {
+		for (let i = 0; i < x.length; i++) {
+			if (x[i] != "") {
+				document.getElementsByTagName("div")[`${i}`].appendChild(document.createElement("p")).innerHTML = x[i].value;
+			}
 		}
+		creado = true;
 	}
 }
