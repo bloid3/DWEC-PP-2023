ef971e2837b7b01500dc6549df107244ae71c7a8
bloid3 <pablo.pereira00@gmail.com>
2022-10-28 11:06:52 +0200

    Subiendo código

--- ud4/4-8.js
+++ ud4/4-8.js
@@ -7,7 +7,8 @@ function aficiones() {
 		if (divs[i].getElementsByTagName("p").length == 0) {
 			if (x[i] != "") {
 				divs[`${i}`].appendChild(document.createElement("p")).innerHTML = x[i].value;
-				divs[`${i}`].getElementsByTagName("p").className = "azul";
+				divs[`${i}`].getElementsByTagName("p")[0].setAttribute("class", "azul")
+				divs[`${i}`].getElementsByTagName("h1")[0].className("verde")
 			}
 		} else {
 			divs[i].getElementsByTagName("p")[0].innerHTML = x[i].value;
