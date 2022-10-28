c8acfb3ae5aeb2afb9d6630753ed37d6c21c3442
bloid3 <pablo.pereira00@gmail.com>
2022-10-28 09:11:53 +0200

    Subiendo código

---
+++ ud4/4-8.js
@@ -0,0 +1,5 @@
+function aficiones() {
+	document.getElementById("res_dep").innerHTML = document.getElementById("deporte").value;
+	document.getElementById("res_ser").innerHTML = document.getElementById("serie").value;
+	document.getElementById("res_pel").innerHTML = document.getElementById("pelicula").value;
+}
