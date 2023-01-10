bf5b8b2ba8e6419608d926c4cf6a438153c67707
bloid3 <pablo.pereira00@gmail.com>
2023-01-10 12:26:17 +0100

    Subiendo código

---
+++ 2ªEvaluacion/ud5/Ejemplo-Acceso/js/formulario.js
@@ -0,0 +1,132 @@
+/*********************************************************************************************
+/*********************************************************************************************
+        Rellenar el valor del campo nombre utilizando un "prompt" para introducir los datos
+*********************************************************************************************
+*********************************************************************************************/
+
+document.getElementById('id_input_text').addEventListener("click",(event) => {
+	//Utilizando el objeto event
+  event.target.value = prompt("Introduce tu nombre: ");
+  //Utilizando this
+  //this.value = promtp("Introduce tu nombre: ");7});
+
+
+/*********************************************************************************************
+*********************************************************************************************
+    Modificar el color del fondo del formulario
+*********************************************************************************************
+*********************************************************************************************/
+for(let e of document.getElementsByName('form-color')){
+  e.addEventListener("click",(event) => {
+    document.getElementById('id_form').style.backgroundColor = event.target.value;
+  });
+}  
+
+
+/*********************************************************************************************
+*********************************************************************************************
+     Resetear el formulario
+*********************************************************************************************
+*********************************************************************************************/
+document.forms[0]['reset'].addEventListener("click",function() {
+  
+  alert("Empezamos de nuevo");
+  document.getElementById("answers").innerHTML="<h2>Respuestas</h2>";
+ 
+});
+
+
+/*********************************************************************************************
+*********************************************************************************************
+      Recoger, recorrer y mostrar el contenido del formulario
+*********************************************************************************************
+*********************************************************************************************/
+document.forms[0].addEventListener("submit",(event) => {
+  
+  document.getElementById("answers").innerHTML="<h2>Respuestas</h2>";
+  
+  for(let e of document.forms[0].elements){
+    let contentText = "";
+    let container = document.createElement("p");
+    
+    switch(e.type){
+      case "checkbox":
+        if(e.checked) contentText+=`${e.name}: ${e.value}`;
+        break;
+      case "radio":
+        if(e.checked) contentText+=`${e.name}: ${e.value}`;
+        break;  
+      case "color":
+        if(e.value!="") contentText+=`${e.name}: ${e.value}`;
+        break;  
+      default:
+        if(e.value!="" && e.type!="submit" && e.type!="reset") contentText+=`${e.name}: ${e.value}`;
+        break;    
+    }
+
+    if(e.type=="color") container.style.color = `${e.value}`;
+    container.appendChild(document.createTextNode(contentText));
+    document.getElementById("answers").appendChild(container);
+  }
+
+  event.preventDefault();
+})
+
+/*********************************************************************************************
+*********************************************************************************************
+      Cambio dinámico del día preferente
+*********************************************************************************************
+*********************************************************************************************/
+let dias = new Set();
+
+for(let e of document.getElementsByClassName('day-available')){
+
+  e.addEventListener("click",(event) => {
+    let diaMarcado = event.target.id.substring(9);
+    if(dias.has(diaMarcado)){
+      dias.delete(diaMarcado);
+    }else{
+      dias.add(diaMarcado);
+    }
+    let selectDiaPreferente = document.forms[0].elements['id_select'];
+    
+    if(dias.size == 0 ){
+      selectDiaPreferente.innerHTML = '<option value="" selected disabled>Ningún elemento seleccionado</option>';
+    }else{
+      selectDiaPreferente.innerHTML = "";
+    }
+
+    let i = 0;
+    for(d of dias){
+      let textday = "";
+      switch(d){
+        case "monday":
+          textday = "Lunes";
+          break;
+        case "tuesday":
+          textday = "Martes";
+          break;
+        case "wednesday":
+          textday = "Miércoles";
+          break;
+        case "thursday":
+          textday = "Jueves";
+          break;
+        case "friday":
+          textday = "Viernes";
+          break;
+        default:
+          break;          
+      }
+      let contentText = document.createTextNode(textday);  
+      let container = document.createElement("option");
+      container.appendChild(contentText);
+      container.value = d;
+      if(i == dias.size - 1 ){
+        container.selected = true;
+      }
+      selectDiaPreferente.appendChild(container);
+      i++;
+    }
+  });
+}  })
---
+++ 2ªEvaluacion/ud5/Estructura-doble-validación/javascript.js
@@ -0,0 +1,93 @@
+
+// Al hacer click en el botón de enviar tendrá que llamar a la la función validar 
+// que se encargará de validar el formulario.
+document.getElementById("idEnviar").addEventListener('click', validar, false);
+let formulario=document.forms["idFormulario"]
+
+/****************************************************************
+ * FUNCIÓN GENERAL DE VALIDACI�N
+ * Unificar los dos tipos de validación
+ *          - Mediante JS
+ *          - Mediante Api de acceso a validación de html
+ ****************************************************************/
+function validar(e){
+   // IMPORTANTE!!! Realizar limpieza del formulario, a nivel de ClassName
+   
+   // IMPORTANTE!!! Realizar limpieza del formulario, a nivel de Spam de error
+
+   // IMPORTANTE!!! Deshabilitamos el botón
+  
+
+    if(validarAPIHTML(e) && validarJS(e) && confirm("¿Deseas enviar el formulario?")){
+
+        return true;
+
+    }else{
+        e.preventDefault();
+        this.disabled = false;
+        return false  
+    }
+}
+
+
+/********************************************************************************************
+ *******************************************************************************************
+ *************** FUNCIÓN PARA VALIDAR DE FORMA MANUAL MEDIANTE JAVASCRIPT
+ *******************************************************************************************
+ *******************************************************************************************/
+function validarJS(eventopordefecto) {
+    
+    // Validamos cada uno de los apartados con llamadas a sus funciones correspondientes.
+    return validarNombre() && validarEdad();             
+}
+/****************************************************************************
+ * FUNCIÓN INDIVIDUALES DE CADA CAMPO
+ /***************************************************************************/
+
+function validarNombre() {
+    let inputNombre= formulario.elements["idNombre"]
+
+    if (inputNombre.value == ""){        
+        formulario.elements["idNombre"].className = "error";
+        formulario.elements["idNombre"].focus();
+        document.getElementById('idNombreError').innerHTML="El campo: " + formulario.elements["idNombre"].name + ", es requerido"
+        
+        return false
+    }
+    return true
+}
+
+/***************************************************************************/
+/***************************************************************************/
+
+function validarEdad() {
+ 
+    let inputEdad=formulario.elements["idEdad"]
+    
+    // IMPORTANTE!! Realizar la validación de la edad mediante javascript.
+    return true;
+}
+  
+/***************************************************************************/
+/***************************************************************************/
+
+function validarMatricula() {
+    
+    let inputMatricula=formulario.elements["idMatricula"]
+
+    // IMPORTANTE!! Realizar la validación de la matrícula mediante javascript y utilizando expresiones regulares
+
+    return true;
+}
+
+
+/********************************************************************************************
+ *********************************************************************************************
+ ******** FUNCIÓN PARA VALIDAR, MEDIANTE EL USO DE LA API DE ACCESO A LA VALIDACIÓN DE HTML 
+ *******************************************************************************************
+ ********************************************************************************************/
+
+function validarAPIHTML(eventopordefecto) {   
+
+    return true;
+}
